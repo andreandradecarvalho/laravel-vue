8896614b5fccfb0860035a7eeef56146c5e34352
Andre Andrade Carvalho <andreandrade17@gmail.com>
2025-05-03 17:15:09 -0300

    add axios

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,11 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import '@/plugins/vee-validate'
+import '@/plugins/axios'
 
 const app = createApp(App).use(store).use(router)
 
